04e05f8010b8ab7e4ffcf2de4f33f806336186f6
David Miller <david@experience7.com>
2023-12-10 14:29:17 +0100

    initial conversion to biofoodtruck content and data

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,50 +1,74 @@
 /**
  * SEO component that queries for data with
  * Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
  */
 
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
 const Seo = ({ description, title, children }) => {
-  const { site } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            description
-            social {
-              twitter
+    const { site } = useStaticQuery(
+      graphql`
+        query {
+          site {
+              siteMetadata {
+                title
+                author {
+                  name
+                  summary
+                }
+                description
+                siteBrand
+                siteUrl
+                siteImage
+                keywords
+                social {
+                  twitterUsername
+                  twitterUrl
+                  instagramUrl
+                  facebookUrl
+                }
+              }
             }
           }
+        `
+      )
+  
+      const defaultDescription = description || site.siteMetadata.description
+      const defaultTitle = site.siteMetadata?.title
+      const pathName = `${site.siteMetadata?.siteUrl}/${title.toString().toLowerCase()}`
+      const seo = {
+          title: title || defaultTitle,
+          description: description || defaultDescription,
+          image: `${site.siteMetadata.siteUrl}/${site.siteMetadata.image}`,
+          url: `${site.siteMetadata.siteUrl}${pathName || ``}`,
+          twitterName: site.siteMetadata?.social?.twitterUsername || ``,
+          twitterUrl: site.siteMetadata?.social?.twitterUrl || ``,
+          instagramUrl: site.siteMetadata?.social?.instagramUrl || ``,
+          facebookUrl: site.siteMetadata?.social?.facebookUrl  || ``
         }
-      }
-    `
-  )
-
-  const metaDescription = description || site.siteMetadata.description
-  const defaultTitle = site.siteMetadata?.title
-
-  return (
-    <>
-      <title>{defaultTitle ? `${title} | ${defaultTitle}` : title}</title>
-      <meta name="description" content={metaDescription} />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={metaDescription} />
-      <meta property="og:type" content="website" />
-      <meta name="twitter:card" content="summary" />
-      <meta
-        name="twitter:creator"
-        content={site.siteMetadata?.social?.twitter || ``}
-      />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={metaDescription} />
-      {children}
-    </>
-  )
-}
-
-export default Seo
+  
+      return (
+          <>
+              <title>{defaultTitle ? `${title} | ${defaultTitle}` : title}</title>
+              <link rel="canonical" href={pathName} />
+              <meta name="description" content={seo.description} />
+              <meta name="image" content={seo.image} />
+              <meta property="og:title" content={title} />
+              <meta property="og:description" content={seo.description} />
+              <meta property="fb:app_id" content={seo.facebookUrl} />
+              <meta property="og:type" content="website" />
+              <meta property="og:site_name" content="biofoodtruck"></meta>
+              <meta name="twitter:card" content="summary_large_image" />
+              <meta name="twitter:creator" content={seo.twitterName || ``} />
+              <meta name="twitter:title" content={title} />
+              <meta name="twitter:description" content={seo.description} />
+              <meta name="twitter:url" content={seo.twitterUrl} />
+              <meta name="twitter:site" content={seo.twitterUrl} />
+              <meta name="twitter:image:alt" content="biofoodtruck logo" />
+              {children}
+          </>
+      )
+  }
+  
+  export default Seo
