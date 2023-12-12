341311eb14ea77ebc7317e28c851ec42a225bcfd
David Miller <david@experience7.com>
2023-12-12 23:50:10 +0100

    add images and josefin san font

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,6 @@
+// import Josefin sans font
+import "@fontsource/josefin-sans"
+import "@fontsource/josefin-sans/700.css"
 // normalize CSS across browsers
 import "./src/normalize.css"
 // custom CSS styles
